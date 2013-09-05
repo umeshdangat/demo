26ca08c573a48767dfa2dd4f7d24e8a0484649cc
Umesh Dangat <umeshdangat@Umeshs-MacBook-Air.local>
2013-09-04 22:04:36 -0700

    removing unused import

--- temp.py
+++ temp.py
@@ -1,7 +1,7 @@
 from logging.handlers import RotatingFileHandler
 from datetime import datetime
 import os
-import sys
+
 def test(maxBytes,backupCount,encoding):
 	pid = os.getpid()
 	filename = datetime.now().strftime('%y_%m_%d_%T.%f_'+ str(pid)) + '_analysis.log'
